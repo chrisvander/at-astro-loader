3553a80154728f13c30b85a5515469911ec52a5b
Christian van der Loo <chris.vanderloo@icloud.com>
2026-04-20 20:22:44 -0400

    chore: use `tsgo` to build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
@@ -13,5 +12,4 @@ export default defineConfig({
       external: ["astro", "zod/mini"],
     },
   },
-  plugins: [dts()],
 });
